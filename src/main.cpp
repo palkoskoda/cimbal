51fa5506fe7b5bf6ff39af8aa20700edea03d193
palkoskoda <37327744+palkoskoda@users.noreply.github.com>
2022-07-17 16:37:32 +0200

    porotyp na 4 struny

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,99 @@
 #include <Arduino.h>
 /*
- * MIDIUSB_loop.ino
- *
- * Created: 4/6/2015 10:47:08 AM
- * Author: gurbrinder grewal
- * Modified by Arduino LLC (2015)
- */ 
+    ArduinoPrintADC.ino
+    Author: Seb Madgwick
+    Sends up to all 6 analogue inputs values in ASCII as comma separated values
+    over serial.  Each line is terminated with a carriage return character ('\r').
+    The number of channels is sent by sending a character value of '1' to '6' to 
+    the Arduino.
+    
+    Tested with "arduino-1.0.3" and "Arduino Uno".
+ */
 
-#include "C:\Users\palko\Documents\PlatformIO\Projects\seeduino_cimbal\.pio\libdeps\seeed_xiao\MIDIUSB\src\MIDIUSB.h"
+#include <stdlib.h> // div, div_t
 
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
-// Second parameter is note-on/note-off, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
+#define POCET_STRUN 2
+#define CRASH_TOLERANCE 10
+
+
+typedef struct {
+    int pin=0;
+    float signal_avg=0;
+    int edge_down_number;
+    int edge_max_hold;
+    bool flag1=false;
+    bool flag2=false;
+    bool falling_edge_detector=false;
+    unsigned long time_flag=0;
+    int note;
+    int velocity2send;
+} struny;
+struny struna[POCET_STRUN];
+
+void send_notes () {
+    for (int i=0; i<POCET_STRUN; i++) {
+        if(struna[i].velocity2send>0){
+            Serial.write(0x91); //cmd
+            Serial.write(struna[i].note); //pitch
+            Serial.write(struna[i].velocity2send); //velocity
+            Serial.write(0x81); //cmd
+            Serial.write(struna[i].note); //pitch
+            Serial.write(0); //velocity
+            struna[i].velocity2send=0;
+        }
+    }
 }
-
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
-}
-
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+    struna[0].pin=A4;
+    struna[0].note=50;
+    struna[1].pin=A1;
+    struna[1].note=54;
 
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
-
-void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-  MidiUSB.sendMIDI(event);
+    Serial.begin(115200);
+    for (int i=0; i<POCET_STRUN; i++) { 
+        pinMode(struna[i].pin, INPUT);
+        struna[i].signal_avg=analogRead(struna[i].pin);
+    } //nacita prvu hodnotu, aby sa priemer prilis nelisil
 }
 
 void loop() {
-  //MidiUSB.accept();
-  //delayMicroseconds(1);
-  midiEventPacket_t rx;
-  do {
-    rx = MidiUSB.read();
-    if (rx.header != 0) {
-      //send back the received MIDI command
-      MidiUSB.sendMIDI(rx);
-      MidiUSB.flush();
+    for (int j=0; j<POCET_STRUN; j++){
+
+        int signal_read=analogRead(struna[j].pin);
+        int signal=signal_read-struna[j].signal_avg;
+
+        if (struna[j].edge_down_number<3) {
+            if (struna[j].edge_max_hold<signal) struna[j].edge_max_hold = signal;
+            else struna[j].edge_down_number++;
+            goto koniec;
+        } //skratka ked sleduje zostupnu hranu
+        else if (struna[j].edge_down_number==3) {
+            int vystup=(signal*1.5)+38;
+            vystup = max(vystup, 30);
+            vystup = min(vystup, 126);
+            struna[j].edge_down_number++;
+            struna[j].velocity2send=vystup;
+        }
+
+        unsigned long myTime = millis();        
+        if (signal>2 && struna[j].flag1==0) {
+            struna[j].flag1=true; 
+            struna[j].flag2=true; 
+            struna[j].time_flag=myTime;      
+            struna[j].edge_down_number=0;
+        }
+
+        if (struna[j].flag2==false) {struna[j].signal_avg=(49*struna[j].signal_avg + signal_read) / 50;}
+        struna[j].signal_avg=(999*struna[j].signal_avg + signal_read) / 1000;
+
+        if ((struna[j].flag1==true) && (myTime - struna[j].time_flag)>30) {struna[j].flag1=0;}
+        if ((struna[j].flag2==true) && (myTime - struna[j].time_flag)>300) {struna[j].flag2=0;}
+        
     }
-  } while (rx.header != 0);
+    send_notes();
+
+    koniec:
+    ;
+
 }
